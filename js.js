8d67fa4cd118b51bd1304914e5b546a96c6c5ecb
Moshimota <dasercasseigzez@gmail.com>
2023-04-01 18:34:12 +0500

    siuuuuuuuuuuuuuu

---
+++ js.js
@@ -0,0 +1,39 @@
+//ex 3
+// function func(a,b) {
+//     if (a.includes(b)) {
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+// console.log(func("world","wor"));
+
+// ex 4
+// const person = {
+//     name: "John Doe",
+//     email: "john.doe@example.com"
+// };
+//   function formatPerson(person) {
+//     const {name, email} = person;
+//     return `${name}: ${email}`;
+// }
+// console.log(formatPerson(person));
+
+// ex 5
+  
+// function filterUpperCaseStrings(array) {
+//     return array.filter((str) => str.toUpperCase() === str);
+// }
+// console.log(filterUpperCaseStrings(['arr', 'ASDF', 'qwerASDF']));
+
+// ex 2
+
+// function findLargestNumber(arr) {
+//     let largestNum = arr[0];
+//     for (let i = 1; i < arr.length; i++) {
+//       if (arr[i] > largestNum) {
+//         largestNum = arr[i];
+//       }
+//     }
+//     return largestNum;
+// }
